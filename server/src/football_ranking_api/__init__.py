ee8a2130abd92756d350f2f7301d0daaf3975022
phcreery <phcreery@gmail.com>
2024-10-07 08:47:45 -0500

    llow cors

--- server/src/football_ranking_api/__init__.py
+++ server/src/football_ranking_api/__init__.py
@@ -5,11 +5,20 @@ import httpx
 import asyncio
 from .scores import request
 from .rank import rank
+
 # from fastapi.routing import APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(root_path="/api/v1")
 # router = APIRouter()
 # app.include_router(router, prefix="/api/v1")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
